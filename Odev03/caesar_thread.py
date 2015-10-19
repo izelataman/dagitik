433f364015df5a229f90f6413f7046628044b70c
izelataman <nurizelataman@gmail.com>
2015-10-19 23:21:23 +0300

    Update caesar_thread.py
    thread'siz, dosyadan okuyor, şifreleyip dosyaya yazıyor.

--- Odev03/caesar_thread.py
+++ Odev03/caesar_thread.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Mon Oct 19 19:04:06 2015
 
-This is a temporary script file.
+@author: Toshiba-PC
 """
-from random import uniform
-import random
+
+l = int(input("kac karakter: "))
+
+parca =[]
+counter = 0
+fo = open("C:\Users\Toshiba-PC\Desktop\Dagitik\metin.txt","r")
+text = fo.read(l)
+while text != "":
+    parca.append(text)
+    counter += l
+    fo.seek(counter)
+    text = fo.read(l)
+    
+print(parca)
+fo.close()
+
+
+fh = open("C:\Users\Toshiba-PC\Desktop\Dagitik\sifre.txt","w")
+for i in range(len(parca)):
+    fh.write(parca[i])
+fh.close()
+
 
 alfabe = list("abcdefghijklmnopqrstuvwxyz")
 #s = random.randint(1,25)
@@ -18,18 +38,21 @@ for i in range(0,26):
     anahtar[(i + s) % 26] = alfabe[i]
 
 anahtar = [element.upper() for element in anahtar]
-print(anahtar)
+#print(anahtar)
+fm = open("C:\Users\Toshiba-PC\Desktop\Dagitik\sifre.txt","r")
+sifrelenecekmetin = fm.read()
+fm.close()
 
-metin = "lorem ipsum dolor sit amet"
+print(sifrelenecekmetin)
+#metin = "lorem ipsum.Dolor!- sit amet"
 sifre = ""
-
-for i in range(0,26):
-    if(any(map(lambda each: each in alfabe, metin[i]))):
-        sifre += anahtar[(alfabe.index(str(metin[i])))]
+lower = sifrelenecekmetin.lower()
+for i in range(0,len(lower)):
+    if(any(map(lambda each: each in alfabe, lower[i]))):
+        sifre += anahtar[(alfabe.index(str(lower[i])))]
     else:
-        sifre += " "
-print(sifre)
-        
-    
-#print(alfabe.index(str(metin[6])))
-    
+        sifre += lower[i]
+
+fk =  open("C:\Users\Toshiba-PC\Desktop\Dagitik\sifre.txt","w")
+fk.write(sifre)
+fk.close()
