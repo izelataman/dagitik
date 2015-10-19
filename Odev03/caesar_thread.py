ea88d60160c33d1023d87ec3ab347921c38e86ca
izelataman <nurizelataman@gmail.com>
2015-10-19 17:09:49 +0300

    Update caesar_thread.py

--- Odev03/caesar_thread.py
+++ Odev03/caesar_thread.py
@@ -1,26 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
 from random import uniform
 import random
 
-alfabe = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-kaydirma = random.randint(1,26)
+alfabe = list("abcdefghijklmnopqrstuvwxyz")
+#s = random.randint(1,25)
+s = 7
+#print(s)
+
 anahtar = [None]*26
 
-print(kaydirma)
-#print(alfabe[5])
-
 for i in range(0,26):
-    anahtar[((i + kaydirma) % 26)] = alfabe[i]
-        
-print(alfabe)
+    anahtar[(i + s) % 26] = alfabe[i]
+
+anahtar = [element.upper() for element in anahtar]
 print(anahtar)
 
 metin = "lorem ipsum dolor sit amet"
 sifre = ""
-print(alfabe.index('l'))
-print(metin[0])
 
-for i in metin:
-    l = (alfabe.index(i) + kaydirma) % 26
-    sifre += alfabe[l]
-    
+for i in range(0,26):
+    if(any(map(lambda each: each in alfabe, metin[i]))):
+        sifre += anahtar[(alfabe.index(str(metin[i])))]
+    else:
+        sifre += " "
 print(sifre)
+        
+    
+#print(alfabe.index(str(metin[6])))
+    
