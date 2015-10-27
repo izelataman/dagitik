4704e6b042d4c0bf9efed14274a28935cd224343
izelataman <nurizelataman@gmail.com>
2015-10-27 15:52:35 +0200

    Create server.py

---
+++ Odev04/server.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 13:17:23 2015
+
+@author: Toshiba-PC
+"""
+import socket
+import threading
+import time
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, clientSocket, clientAddr):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.clientSocket = clientSocket
+        self.clientAddr = clientAddr
+    def run(self):
+        print "Starting Thread-" + str(self.threadID)
+        todo(self.clientSocket, self.clientAddr)
+        print "Ending Thread-" + str(self.threadID)
+        
+def todo(clientSocket,clientAddr):
+    print 'todo yu cagirdi'
+    tLock.acquire()
+    clientSocket.send('Merhaba, saat şu an 15,15')
+    tLock.release()
+    time.sleep(1)
+        
+threadCounter = 0     
+s = socket.socket()
+host = "0.0.0.0"
+port = 12345
+s.bind((host, port))
+s.listen(5)
+tLock = threading.Lock()
+threads = []
+
+while True:
+    print "Waiting for connection"
+    clientSocket, clientAddr = s.accept()
+    print 'Got a connection from ', clientAddr
+    thread = myThread(threadCounter, clientSocket, clientAddr)
+    thread.start()
+    threads.append(thread)
+    threadCounter += 1
+
+    for t in threads: 
+        t.join()
+    print "Exiting Main Thread \n"
