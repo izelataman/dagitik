856b16c3072b643689c966415eb8ed0ca5a39bd1
izelataman <nurizelataman@gmail.com>
2015-10-28 11:11:58 +0200

    Update client.py
    Bad File Descriptor hatası çözüldü

--- Odev04/client.py
+++ Odev04/client.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 13:20:18 2015
+
+@author: Toshiba-PC
+"""
+"""
+#!/usr/bin/env python
+import socket
+s = socket.socket()
+host = "127.0.0.1"
+port = 12345
+s.connect((host, port))
+print s.recv(1024)
+"""
 
 #!/usr/bin/env python
 import socket
@@ -5,7 +20,6 @@ import threading
 import random
 import sys
 import time
-
 class readThread (threading.Thread):
     def __init__(self, threadID, clientSocket, clientAddr):
         threading.Thread.__init__(self)
@@ -28,48 +42,35 @@ class writeThread (threading.Thread):
         self.clientAddr = clientAddr
     def run(self):
          print "Starting writeThread " + self.name
-         
+         tLock.acquire()
          while True:
             for index in xrange(15):
                 data = raw_input("zzzz:")
-                tLock.acquire()
                 s.send(data)
-                if(data == "disconnect"):
+                while s.recv(2048) != "ok":
                     print "Disconnecting"
                     s.close()
-                print s.recv(2048)+ "   <" + str(host) + ">",
-                tLock.release()
+                    sys.exit("Received disconnect message.  Shutting down.")
+                print "PEKI" + "    <" + str(host) + ">"
+               
          print "Exiting writeThread " + self.name 
-         
+         tLock.release()
          time.sleep(1)
          
 tLock = threading.Lock()
-rThreadCounter = 0
-wThreadCounter = 0
-rthreads = []
-wthreads = []
+threadCounter = 0
 s = socket.socket()
 host = "127.0.0.1"
 hostserver = "0.0.0.0"
 port = 12345
 s.connect((host, port))
 
-while True:
-        
-    rThread = readThread(rThreadCounter, s, host)
-    rThread.start()
-    rThreadCounter += 1
-    rthreads.append(rThread)
 
-    
-    wThread = writeThread(wThreadCounter, s, host)
-    wThread.start()
-    wThreadCounter += 1
-    wthreads.append(wThread)
-    
-    for t in rthreads: 
-        t.join()
-    for t in wthreads: 
-        t.join()
-    
-    
+rThread = readThread(threadCounter, s, host)
+rThread.start()
+rThread.join()
+
+wThread = writeThread(threadCounter, s, host)
+wThread.start()
+wThread.join()
+
