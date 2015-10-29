4e726cdc1e4c389ac4af0daa624c86aa0d6ba459
izelataman <nurizelataman@gmail.com>
2015-10-29 19:18:55 +0200

    Update server.py

--- Odev04/server.py
+++ Odev04/server.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 27 13:17:23 2015
+Created on Thu Oct 29 16:53:13 2015
 
 @author: Toshiba-PC
 """
+
+
 import socket
 import threading
 import time
+import random
 
 class myThread (threading.Thread):
     def __init__(self, threadID, clientSocket, clientAddr):
@@ -18,23 +21,28 @@ class myThread (threading.Thread):
         print "Starting Thread-" + str(self.threadID)
         todo(self.clientSocket, self.clientAddr)
         print "Ending Thread-" + str(self.threadID)
-        
+    
 def todo(clientSocket,clientAddr):
     print 'todo yu cagirdi'
+    clientSocket.send("Welcome")
     tLock.acquire()
     while True:
+        r = random.randint(2,120)
+        print r
+        if(r % 5 == 0):
+            clientSocket.send("Su an saat: ")
         output = clientSocket.recv(2048);
         if output.strip() == "disconnect":
             clientSocket.close()
             return 0
-            #clientSocket.send("dack")
         elif output:
             print "Message received from client:"
             print output
             clientSocket.send("ok")
     tLock.release()
     time.sleep(1)
-        
+
+r = 0      
 threadCounter = 0     
 s = socket.socket()
 host = "0.0.0.0"
