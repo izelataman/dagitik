57d0871dcce3f63dbd470885af03803749dcd229
izelataman <nurizelataman@gmail.com>
2015-12-26 13:56:53 +0200

    Update peer.py

--- proje/peer.py
+++ proje/peer.py
@@ -1,1 +1,51 @@
 
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 26 13:35:37 2015
+
+@author: Toshiba-PC
+"""
+import Queue
+import time
+import socket
+import sys
+import threading
+
+class peerServerThread (threading.Thread):
+    def __init__(self, ip, port, socket, cQueue):
+        
+        
+    def run():
+        
+class peerServerSendMessage(threading.Thread):
+    def __init__(self, ip, port, socket, cQueue):
+        
+        
+    def run():
+        
+class peerServerRecieveMessage(threading.Thread):
+    def __init__(self, ip, port, socket, cQueue):
+        
+        
+    def run():
+        
+class peerClientThread (threading.Thread):
+    def __init__(self, ip, port, socket, cQueue):
+        
+        
+    def run():
+    
+class peerClientSendMessage(threading.Thread):
+    def __init__(self, ip, port, socket, cQueue):
+        
+        
+    def run():
+
+class peerClientRecieveMessage(threading.Thread):
+    def __init__(self, ip, port, socket, cQueue):
+        
+        
+    def run():
+
+    
+
