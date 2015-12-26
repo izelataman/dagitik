813090d0c83007454546189463a0caa05fe6a548
izelataman <nurizelataman@gmail.com>
2015-12-26 14:57:56 +0200

    Update peer.py

--- proje/peer.py
+++ proje/peer.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 """
 Created on Sat Dec 26 13:35:37 2015
@@ -12,11 +11,14 @@ import sys
 import threading
 
 class peerServerThread (threading.Thread):
-    def __init__(self, ip, port, socket, cQueue):
-        
-        
-    def run():
-        
+    def __init__(self, ip, port, socket):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.ip = ip
+        self.port = port
+    def run(self):
+        self.socket.connect((self.ip, self.port))
+
 class peerServerSendMessage(threading.Thread):
     def __init__(self, ip, port, socket, cQueue):
         
@@ -46,6 +48,14 @@ class peerClientRecieveMessage(threading.Thread):
         
         
     def run():
+        
 
-    
+s = socket.socket()
+host = "127.0.0.1"
+port = 12354
+hostserver = "0.0.0.0"
+
+serverThread = peerServerThread(host,port,s)
+serverThread.start()
+serverThread.join()
 
